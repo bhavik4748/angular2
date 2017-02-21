b9b220528578e0a7524ef86a5ba23442db7fdcd8
Bhavik Gosrani <Bhavik Gosrani>
2017-02-20 22:24:24 -0500

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ProductService } from './products/product.service'
+
+@Component({
+    selector: 'pm-app',
+    moduleId: module.id,
+    templateUrl:'app.component.html' ,
+    providers: [ProductService]
+
+})
+export class AppComponent {
+    pageTitle: string = `Acme Product Managment`;
+}
